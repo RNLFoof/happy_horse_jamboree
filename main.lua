48b3b8badc9e9c3ed1d50a3039fc6f82f6b6174c
RNLFoof <z.zablotsky@gmail.com>
2025-03-03 19:29:13 -0500

    feature: made psychostasia cards line up

--- main.lua
+++ main.lua
@@ -74,3 +74,44 @@ function Card:remove_from_deck()
 
     return ref(self)
 end
+
+local ref = CardArea.align_cards
+function CardArea:align_cards()
+    ref(self)
+
+    -- Aligns jokers while taking their size into account. Based on the actual joker alignment code 
+    if self.config.type == 'joker' and G.GAME.starting_params.mig_psychostasia then
+        psychostasia_k = 0
+        effective_card_count = 0
+        for _, card in ipairs(self.cards) do
+            effective_card_count = effective_card_count + card.config.center.rarity
+        end
+        ecc = effective_card_count
+        previous_psychostasia_k = 0 -- Replaces k-1
+
+        for k, card in ipairs(self.cards) do
+            psychostasia_k = psychostasia_k + card.config.center.rarity
+            -- self.card_width is the actual width that cards have, potentially modified per CardArea. I'm going to just assume it's normal 
+            -- This can't be set as just the card's actual width, because of wee joker and maybe others
+            width_of_this_card = card.config.center.rarity*0.5*self.card_w --
+            if not card.states.drag.is then 
+                card.T.r = 0.1*(-ecc/2 - 0.5 + psychostasia_k)/(ecc)+ (G.SETTINGS.reduced_motion and 0 or 1)*0.02*math.sin(2*G.TIMERS.REAL+card.T.x)
+                if ecc > 2 or (ecc > 1 and self == G.consumeables) or (ecc > 1 and self.config.spread) then
+                    card.T.x = 
+                        -- The card area's own X
+                        self.T.x
+                        + (self.T.w-self.card_w*0.5)*(
+                            (previous_psychostasia_k)/(ecc-1)
+                        )
+                        -- Centers the card within the space allocated to it 
+                        + 0.5*(width_of_this_card - card.T.w)
+                end
+                local highlight_height = G.HIGHLIGHT_H/2
+                if not card.highlighted then highlight_height = 0 end
+                card.T.y = self.T.y + self.T.h/2 - card.T.h/2 - highlight_height+ (G.SETTINGS.reduced_motion and 0 or 1)*0.03*math.sin(0.666*G.TIMERS.REAL+card.T.x)
+                card.T.x = card.T.x + card.shadow_parrallax.x/30
+            end
+            previous_psychostasia_k = psychostasia_k
+        end
+    end
+end
