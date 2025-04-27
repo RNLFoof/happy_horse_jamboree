083b703156d167c078eff0db286e5a2f763821e0
RNLFoof <z.zablotsky@gmail.com>
2025-04-27 16:33:12 -0400

    one last rename oooooooo

--- behavior/tags/horse_luck/horse_luck.lua
+++ behavior/tags/horse_luck/horse_luck.lua
@@ -6,6 +6,6 @@ SMODS.Tag({ key = "horse_luck", loc_txt = { name =
 "Shop has a {C:attention}horse{} :)" } }, atlas = 
 
 "mig_atlas_tags", pos = 
-atlas_tags_positions["HORSE LUCK TAG"], no_collection = 
+atlas_tags_positions["horse_luck"], no_collection = 
 true, in_pool = function(self, args)return 
 false end })
