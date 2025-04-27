c3029d1cf483692aff2ee49c937d4bf63256e330
RNLFoof <z.zablotsky@gmail.com>
2025-04-27 15:26:21 -0400

    chore: made the name of the horse test bundle more concise :)

--- behavior/jokers/Horse/test_Horse.lua
+++ behavior/jokers/Horse/test_Horse.lua
@@ -1,9 +1,8 @@
-local testing = assert(SMODS.load_file("libs\\steamodded_test\\main.lua"))()
-assert(SMODS.load_file("libs\\steamodded_test\\test_main.lua"))()local _obj_0 = 
+local testing = assert(SMODS.load_file("libs\\steamodded_test\\main.lua"))()local _obj_0 = 
 
 G.steamodded_tests.tests
 
-_obj_0[#_obj_0 + 1] = testing.TestBundle("Horse tests", { 
+_obj_0[#_obj_0 + 1] = testing.TestBundle("horses", { 
 testing.Test("chipschipschips", { testing.create_state_steps(), function()return 
 testing.play_hand({ "A" }, { centers = "j_mig_horse_chipschipschips" })end, function()return 
 testing.assert_hand_scored(11 + 5 + 24)end }), 
