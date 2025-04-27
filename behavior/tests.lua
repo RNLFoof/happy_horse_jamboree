95b7766454aa5b91c1a880b7f3d02c8b228c3cd1
RNLFoof <z.zablotsky@gmail.com>
2025-04-27 16:20:03 -0400

    oops I submitrted the wrong version of this earlier

--- behavior/tests.lua
+++ behavior/tests.lua
@@ -1,23 +1,15 @@
--- [yue]: ./behavior/tests.yue
-local testing = assert(SMODS.load_file("libs\\steamodded_test\\main.lua"))() -- 1
-local useful_things = assert(SMODS.load_file("useful_things.lua"))() -- 2
-local _obj_0 = G.steamodded_tests.tests -- 4
-_obj_0[#_obj_0 + 1] = testing.TestBundle("misc", { -- 6
-	testing.Test("wrap_method", { -- 7
-		function() -- 7
-			local buildup = { } -- 8
-			local obj = { -- 10
-				fun = function() -- 10
-					buildup[#buildup + 1] = "middle" -- 11
-				end -- 10
-			} -- 9
-			useful_things.wrap_method(obj, "fun", (function() -- 13
-				buildup[#buildup + 1] = "before" -- 13
-			end), (function() -- 13
-				buildup[#buildup + 1] = "after" -- 13
-			end)) -- 13
-			obj.fun() -- 14
-			return testing.assert_eq(table.concat(buildup, "_"), "before_middle_after") -- 15
-		end -- 7
-	}) -- 6
-}) -- 4
+local testing = assert(SMODS.load_file("libs\\steamodded_test\\main.lua"))()local useful_things = 
+assert(SMODS.load_file("useful_things.lua"))()local _obj_0 = 
+
+G.steamodded_tests.tests
+
+_obj_0[#_obj_0 + 1] = testing.TestBundle("misc", { 
+testing.Test("wrap_method", { function()local buildup = 
+{  }local obj = { fun = function()
+
+
+buildup[#buildup + 1] = "middle"end }
+
+useful_things.wrap_method(obj, "fun", (function()buildup[#buildup + 1] = "before"end), (function()buildup[#buildup + 1] = "after"end))
+obj.fun()return 
+testing.assert_eq(table.concat(buildup, "_"), "before_middle_after")end }) })
