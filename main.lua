f2fe28845d306bb0e594e8e5072d3abeccdaee2d
RNLFoof <z.zablotsky@gmail.com>
2025-05-11 20:39:59 -0400

    font stuff

--- main.lua
+++ main.lua
@@ -10,7 +10,13 @@ package.path = 'Mods\\balatro-unknown-mod\\libs\\LuaNES\\?.lua;' .. package.path
 SMODS.Atlas({ key = "modicon", path = 
 "modicon.png", px = 
 32, py = 
-32 })
+32 })local extra_glyphs_font = 
+
+love.graphics.newFont("Mods/" .. SMODS.current_mod.path:gsub("^.+Mods", "") .. "assets/glyphs/m6x11plus_extra_glyphs.ttf", G.TILESIZE * 10)local _list_0 = 
+G.FONTS;for _index_0 = 1, #_list_0 do local font = _list_0[_index_0]
+font.FONT:setFallbacks(extra_glyphs_font)end;local _list_1 = 
+G.LANGUAGES;for _index_0 = 1, #_list_1 do local language = _list_1[_index_0]
+language.font:setFallbacks(extra_glyphs_font)end
 
 local load_all_files_in;load_all_files_in = function(directory)
 directory = directory;local files = 
