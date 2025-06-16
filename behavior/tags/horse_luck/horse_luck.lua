d6b6237775520bfd0b98c379b89abd28327cf1af
RNLFoof <z.zablotsky@gmail.com>
2025-06-16 00:02:28 -0400

    updated a buncha atlases ig? idk what changed

--- behavior/tags/horse_luck/horse_luck.lua
+++ behavior/tags/horse_luck/horse_luck.lua
@@ -8,4 +8,21 @@ SMODS.Tag({ key = "horse_luck", loc_txt = { name =
 "hhj_atlas_tags", pos = 
 atlas_tags_positions["horse_luck"], no_collection = 
 true, in_pool = function(self, args)return 
-false end })
+false end, apply = function(self, tag, context)if not (
+
+context.type == "store_joker_create") then
+return end;local card = 
+
+SMODS.create_card({ set = "Joker", area = 
+context.area, key = 
+"j_hhj_horse_base", key_append = 
+'horse_luck' })
+create_shop_card_ui(card, 'Joker', context.area)
+card.states.visible = false
+tag:yep("HORSE LUCK", G.C.GREEN, function()
+card:start_materialize()
+card.ability.couponed = true;return 
+true end)
+
+tag.triggered = true;return 
+card end })
