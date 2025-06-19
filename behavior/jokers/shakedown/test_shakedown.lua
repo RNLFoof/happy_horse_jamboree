5eb4358daa0fc89ac540ba3f6135c4f99e9653bf
RNLFoof <z.zablotsky@gmail.com>
2025-06-18 21:40:46 -0400

    chore: made shakedown_abuse check for discarding

--- behavior/jokers/shakedown/test_shakedown.lua
+++ behavior/jokers/shakedown/test_shakedown.lua
@@ -7,7 +7,11 @@ _obj_0[#_obj_0 + 1] = TestBundle("shakedown", {
 Test("no_trading_card_abuse", { function()return testing.new_run()end, function()return 
 testing.add_centers({ "j_trading", "j_hhj_shakedown" })end, function()return 
 testing.new_round()end, function()return 
-delay(5)end, function()
+delay(3)end, function()
 
-assert(SMODS.find_card("j_trading").ability.hhj_in_juice_card_until)return 
-testing.assert_eq(SMODS.find_card("j_hhj_shakedown").ability.chips, 0)end }) })
+assert(SMODS.find_card("j_trading")[1].ability.hhj_in_juice_card_until)return 
+testing.assert_eq(SMODS.find_card("j_hhj_shakedown")[0].ability.chips, 0)end, 
+testing.discard_something_steps(), function()return 
+delay(3)end, function()return 
+
+testing.assert_eq(SMODS.find_card("j_trading")[1].ability.hhj_in_juice_card_until, nil)end }) })
