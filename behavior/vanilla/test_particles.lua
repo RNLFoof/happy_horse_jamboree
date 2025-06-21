637739a44d60af946dfe8f4b17c1efaa69367dbc
RNLFoof <z.zablotsky@gmail.com>
2025-06-21 18:45:03 -0400

    bugfix: fixed a test that always failed despite producing the intended result

--- behavior/vanilla/test_particles.lua
+++ behavior/vanilla/test_particles.lua
@@ -33,4 +33,5 @@ testing.assert_eq(dummy.hhj_config.starting_range.max_x, G.CARD_W / 2)end }),
 
 
 testing.Test("no_crash_when_opening_arcana_pack", { testing.create_state_steps(), function()return 
-testing.open_arcana_pack()end }) })
+testing.open_arcana_pack()end, function()return 
+true end }) })
