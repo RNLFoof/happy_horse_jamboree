5da89f6c0bddde3ed5dcd60e459c97f630e26b1b
RNLFoof <z.zablotsky@gmail.com>
2026-01-25 12:36:11 -0500

    chore: comments

--- compile.sh
+++ compile.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+# Delete and recreate the build folder
 rm -r build
 mkdir build
 
+# Compile yue, https://yuescript.org/ 
 yue -t build/ source/
 
+# Copy over raw lua (and NES roms), should be last because it changes directory
 cd source
 find -type f | grep -i -e "lua$" -e "nes$" | xargs -i cp "{}" --parents -t "../build"
